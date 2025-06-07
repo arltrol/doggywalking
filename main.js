b7486877f8dc450b466ebaf64ff8f5f903cfddd6
arltrol <roland.arlt@gmail.com>
2025-06-07 15:59:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,27 +25,22 @@ const game = new Phaser.Game(config);
 function preload() {
     this.load.image('background', 'assets/background.png');
     this.load.image('player', 'assets/player_full_team.png');
-    this.load.image('cpu', 'assets/player_full_team.png');  // temporary CPU sprite
+    this.load.image('cpu', 'assets/player_full_team.png'); // temporary CPU sprite
 }
 
 function create() {
-    // Add background
     this.add.image(400, 300, 'background').setScale(1);
 
-    // Add player (left track)
     player = this.physics.add.sprite(200, 500, 'player');
     player.setScale(0.3);
     player.setCollideWorldBounds(true);
 
-    // Add CPU walker (right track)
     cpu = this.physics.add.sprite(600, 500, 'cpu');
     cpu.setScale(0.3);
     cpu.setCollideWorldBounds(true);
 
-    // Setup keyboard input
     cursors = this.input.keyboard.createCursorKeys();
 
-    // Game title
     this.add.text(20, 20, 'Lea The Best', {
         fontSize: '28px',
         fill: '#222',
@@ -54,7 +49,6 @@ function create() {
 }
 
 function update() {
-    // PLAYER MOVEMENT
     player.setVelocity(0);
     if (cursors.left.isDown) {
         player.setVelocityX(-150);
@@ -67,11 +61,10 @@ function update() {
         player.setVelocityY(150);
     }
 
-    // CPU AUTO-MOVE UP
     cpu.setVelocity(0, -100);
 
-    // Reset CPU to bottom if off screen
-    if (cpu.y < -50) {
-        cpu.y = 650;
+    // 🔧 Tweak reset condition for tall sprite
+    if (cpu.y < -150) {
+        cpu.y = 700;
     }
 }
