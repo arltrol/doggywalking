7358c11bc55dd01b3d67d229d12cd9b0559e6a3b
arltrol <roland.arlt@gmail.com>
2025-06-07 15:45:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-
 const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     backgroundColor: '#aaddaa',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -11,16 +16,26 @@ const config = {
     }
 };
 
+let player;
+let cpu;
+let cursors;
+
 const game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('background', 'assets/background.png');
+    this.load.image('player', 'assets/player_full_team.png');
+    this.load.image('cpu', 'assets/sprites_player2_dogs_obstacles.png');
 }
 
 function create() {
-    this.add.image(400, 300, 'background').setScale(0.5);
-}
+    // Add background
+    this.add.image(400, 300, 'background').setScale(1);
 
-function update() {
-    // Game logic here
-}
+    // Add player (left track)
+    player = this.physics.add.sprite(200, 500, 'player');
+    player.setScale(0.3);
+    player.setCollideWorldBounds(true);
+
+    // Add CPU walker (right track)
+    cpu = this.physics.add.sprite(600, 500, 'cpu');
